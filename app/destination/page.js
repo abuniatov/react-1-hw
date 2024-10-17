7411fdcf3752aff0f9510c8fdf6cbb66c0457b65
abuniatov <abuniatov@gmail.com>
2024-10-17 20:45:23 +0200

    Moved PlanetWishlistItem component to its own file

--- app/destination/page.js
+++ app/destination/page.js
@@ -3,23 +3,12 @@
 import { useState } from 'react';
 
 import styles from '@/components/destination/destination.module.css';
+import { PlanetWishlistItem } from '@/components/destination/PlanetWishlistItem';
 import { AddWishlistItem } from '@/components/destination/AddWishlistItem';
 
 // TASK - React 1 week 2
 // Move this to its own file
-const PlanetWishlistItem = ({
-  name,
-  onRemove,
-  thumbnail,
-}) => {
-  return (
-    <div className={styles.wishlistItem}>
-      <img className={styles.wishlistItemThumbnail} src={thumbnail} alt="" />
-      <b>{name.toUpperCase()}</b>
-      <button onClick={onRemove}>remove</button>
-    </div>
-  );
-}
+
 
 
 export const Destinations = () => {
