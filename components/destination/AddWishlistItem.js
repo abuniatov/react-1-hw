5bd53cdb417b071c33f0117bb56a2edfc5ef35cc
abuniatov <abuniatov@gmail.com>
2024-10-25 15:01:20 +0200

    finished tasks on AddWishlistItem.js file

--- components/destination/AddWishlistItem.js
+++ components/destination/AddWishlistItem.js
@@ -1,15 +1,14 @@
 "use client";
 
-import { useState } from 'react';
-import styles from './destination.module.css';
+import { useState } from "react";
+import styles from "./destination.module.css";
 
-export const AddWishlistItem = ({
-  onAddWishlistItem,
-}) => {
-  const [thumbnail, onThumbnailChange] = useState('/destination/image-europa.png');
+export const AddWishlistItem = ({ onAddWishlistItem }) => {
+  const [thumbnail, setThumbnail] = useState("/destination/image-europa.png");
   // TASK - React 1 week 3
   // 1. Add a useState for the handling the <input id="customWishlist" type="text" />
   // 2. Connect the onThumbnailChange to the <select>
+  const [wishlistItem, setWishlistItem] = useState("");
 
   const onAddItemPressed = () => {
     // TASK - React 1 week 3
@@ -17,22 +16,35 @@ export const AddWishlistItem = ({
     // Clear the <input/> field on button press
     // pass the thumbnail and the name from the input to the onAddWishlistItem function
     // call the onAddWishlistItem here
-  }
+    if (wishlistItem.trim() === "") return;
 
+    onAddWishlistItem(wishlistItem, thumbnail);
+
+    setWishlistItem("");
+  };
 
   return (
     <div className={styles.addWishlistItem}>
       <p>Add custom planet to wishlist</p>
       <label htmlFor="customWishlist">Wishlist item name</label>
-      <input id="customWishlist" type="text" />
+      <input
+        id="customWishlist"
+        type="text"
+        value={wishlistItem}
+        onChange={(e) => setWishlistItem(e.target.value)}
+      />
       <label htmlFor="customWishlistThumbnail">Wishlist item thumbnail</label>
-      <select id="customWishlistThumbnail" >
+      <select
+        id="customWishlistThumbnail"
+        value={thumbnail}
+        onChange={(e) => setThumbnail(e.target.value)}
+      >
         <option value="/destination/image-europa.png">EUROPA</option>
         <option value="/destination/image-mars.png">MARS</option>
         <option value="/destination/image-moon.png">MOON</option>
         <option value="/destination/image-titan.png">TITAN</option>
       </select>
-      <button>ADD CUSTOM</button>
+      <button onClick={onAddItemPressed}>ADD CUSTOM</button>
     </div>
   );
 };
